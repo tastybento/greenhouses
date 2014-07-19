35946a836a53b63113976b0191708aebe90b403b
tastybento <bengibbs@comcast.net>
2014-07-19 11:35:25 -0700

    Updated version number.

--- districts/src/com/wasteofplastic/districts/Districts.java
+++ districts/src/com/wasteofplastic/districts/Districts.java
@@ -7,7 +7,6 @@ import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Calendar;
-import java.util.Date;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
