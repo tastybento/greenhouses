431742b0996ea274cb22123d2c03ad7fe71be710
tastybento <tastybento@wasteofplastic.com>
2016-04-14 18:22:52 -0700

    Updated readme. Updated the text commands like list and recipe.

--- README.md
+++ README.md
@@ -40,11 +40,14 @@ FAQ
 * How do I place a door high up in the wall if the wall is all glass? Place it on a hopper.
 * How do I place a door on a hopper? Crouch and then place it.
 * Can I use metal doors? Yes.
+* Can I use a trap door? Yes.
 * Can I grow swamp flowers with this? Yes. Make a swamp biome and use bonemeal.
 * How much bonemeal is used to grow plants? One per successful plant.
 * How much water do I need to put into the hopper to make it snow? One bucket of water (just the water) is used up every time it rains. This only happens in cold biomes.
 * Can I build a Nether greenhouse? Try it and see... (Actually, you may need permission)
+* Can I build greenhouses in the Nether? Yes. You can colonize the nether with them.
 * What kind of mobs spawn in the biomes? It's what you would expect, wolves in Cold Taiga, horses on plains, etc.
+
 
 Required Plugin
 ---------------
@@ -57,10 +60,12 @@ Installation and Configuration
 3. Place into your plugins folder
 4. Restart your server (or reload plugins)
 5. The plugin will make a folder called Greenhouses. Open that folder.
-6. Check config.yml and edit to be what you want, note the list of world names.
-7. Configure the biomes.yml if you like (advanced) Here is an example one.
-8. Type /gadmin reload in the game to reload the config or restart the server.
-9. Done! To make your first greenhouse, build a glass box and type /g make to see what kind of greenhouse you get. Type /g recipe to see the recipes.
+6. Check **config.yml** and edit to be what you want, note the list of world names.
+7. Configure the **biomes.yml** if you like (advanced).
+8. Type **/gadmin reload** in the game to reload the config or restart the server.
+9. Done!
+
+To make your first greenhouse, build a glass box and type **/g make** to see what kind of greenhouse you get. Type **/g** to see the recipes.
 
 Upgrading
 ---------
@@ -85,19 +90,23 @@ Permissions
 -----------
 Permission to use specific biomes can be added in biomes.yml.
 
-For example, the permission for the Nether (Hell) biome is greenhouses.biome.nether and is set here:
+For example, the permission for the Nether (Hell) biome is **greenhouses.biome.nether** and is set here:
 
  HELL:
+ 
     permission: greenhouses.biome.nether
 
-The permission can also be a rank permission, for example, "myserver.VIPs" or anything you like.
+The permission can be anything you like, e.g., a rank permission, **myserver.VIP**.
 
 General permissions are:
-
+------------------------
   greenhouses.player:
+  
      description: Gives access to player commands
      default: true
+     
   greenhouses.admin:
+  
      description: Gives access to admin commands
      default: op
 
@@ -105,13 +114,14 @@ Stats Disclosure
 ----------------
 This plugin utilizes a plugin metrics system, which means that the following information is collected and sent to mcstats.org:
 
-A unique identifier
-The server's version of Java
-Whether the server is in offline or online mode
-The plugin's version
-The server's version
-The OS version/name and architecture
-The core count for the CPU
-The number of players online
-The Metrics version
+* A unique identifier
+* The server's version of Java
+* Whether the server is in offline or online mode
+* The plugin's version
+* The server's version
+* The OS version/name and architecture
+* The core count for the CPU
+* The number of players online
+* The Metrics version
+
 Visit mcstats.org if you would like to learn more about the stats collected. Go into /plugins/PluginMetrics/config.yml to disable stat collecting if you wish.
--- src/com/wasteofplastic/greenhouses/GreenhouseCmd.java
+++ src/com/wasteofplastic/greenhouses/GreenhouseCmd.java
@@ -24,9 +24,9 @@ public class GreenhouseCmd implements CommandExecutor {
      */
     public GreenhouseCmd(Greenhouses plugin, PlayerCache players) {
 
-	// Plugin instance
-	this.plugin = plugin;
-	this.players = players;
+        // Plugin instance
+        this.plugin = plugin;
+        this.players = players;
     }
     /*
      * (non-Javadoc)
@@ -36,191 +36,200 @@ public class GreenhouseCmd implements CommandExecutor {
      * , org.bukkit.command.Command, java.lang.String, java.lang.String[])
      */
     public boolean onCommand(final CommandSender sender, final Command command, final String label, final String[] split) {
-	if (!(sender instanceof Player)) {
-	    return false;
-	}
-	final Player player = (Player) sender;
-	// Check we are in the right world
-	if (!Settings.worldName.contains(player.getWorld().getName())) {
-	    // notavailable
-	    player.sendMessage(ChatColor.RED + Locale.generalnotavailable);
-	    return true;
-	}
-	// Basic permissions check to even use /greenhouse
-	if (!VaultHelper.checkPerm(player, "greenhouses.player")) {
-	    player.sendMessage(ChatColor.RED + Locale.errornoPermission);
-	    return true;
-	}
-	/*
-	 * Grab data for this player - may be null or empty
-	 * playerUUID is the unique ID of the player who issued the command
-	 */
-	final UUID playerUUID = player.getUniqueId();
-	final Greenhouse greenhouseInNow = players.getInGreenhouse(player);
-	switch (split.length) {
-	// /greenhouse command by itself
-	case 0:
-	    if (greenhouseInNow==null || greenhouseInNow.getOwner().equals(playerUUID)) {
-		player.openInventory(plugin.getRecipeInv(player));
-		return true;
-	    } else {
-		player.sendMessage(ChatColor.RED + Locale.errornotowner);
-		return true;
-	    }
-	case 1:
-	    // /greenhouse <command>
-	    if (split[0].equalsIgnoreCase("help")) { 
-		player.sendMessage(ChatColor.GREEN + Locale.generalgreenhouses +" " + plugin.getDescription().getVersion() + " " + Locale.helphelp + ":");
-		player.sendMessage(ChatColor.YELLOW + "/" + label + " : " + ChatColor.WHITE + Locale.helpopengui);
-		player.sendMessage(ChatColor.YELLOW + "/" + label + " make: " + ChatColor.WHITE + Locale.helpmake);
-		player.sendMessage(ChatColor.YELLOW + "/" + label + " remove: " + ChatColor.WHITE + Locale.helpremove);
-		player.sendMessage(ChatColor.YELLOW + "/" + label + " info: " + ChatColor.WHITE + Locale.helpinfo);
-		player.sendMessage(ChatColor.YELLOW + "/" + label + " list: " + ChatColor.WHITE + Locale.helplist);
-		player.sendMessage(ChatColor.YELLOW + "/" + label + " recipe <number>: " + ChatColor.WHITE + Locale.helprecipe);
-		return true;
-	    } else if (split[0].equalsIgnoreCase("list")) {
-		// List all the biomes that can be made
-		player.sendMessage(ChatColor.GREEN + Locale.listtitle);
-		player.sendMessage(Locale.listinfo);
-		int index = 1;
-		for (BiomeRecipe br : plugin.getBiomeRecipes()) {
-		    player.sendMessage(ChatColor.YELLOW + Integer.toString(index++) + ": " + Util.prettifyText(br.getBiome().toString()));
-		}
-		return true;
-	    } else if (split[0].equalsIgnoreCase("remove")) {
-		if (greenhouseInNow != null) {
-		    if (greenhouseInNow.getOwner().equals(playerUUID)) {
-			player.sendMessage(ChatColor.RED + Locale.errorremoving);
-			plugin.removeGreenhouse(greenhouseInNow);
-			return true;
-		    }
-		    player.sendMessage(ChatColor.RED + Locale.errornotyours);
-		} else {
-		    player.sendMessage(ChatColor.RED + Locale.errornotinside); 
-		}
-		return true;
-	    } else if (split[0].equalsIgnoreCase("make")) {
-		// Sets up a greenhouse
-		if (players.getInGreenhouse(player) != null) {
-		    // alreadyexists
-		    player.sendMessage(ChatColor.RED + Locale.erroralreadyexists);
-		    return true;
-		}
-		// Check if they are at their limit
-		if (plugin.players.isAtLimit(player)) {
-		    player.sendMessage(ChatColor.translateAlternateColorCodes('&', Locale.infonomore));
-		} else {
-		    // Try to make greenhouse
-		    Greenhouse g = plugin.tryToMakeGreenhouse(player);
-		    if (g == null) {
-			// norecipe
-			player.sendMessage(ChatColor.RED + Locale.errornorecipe);
-			return true;
-		    }
-		    // Greenhouse is made
-		}
-		return true;
-	    } else if (split[0].equalsIgnoreCase("info")) {
-		// Show some instructions on how to make greenhouses
-		player.sendMessage(ChatColor.GREEN + ChatColor.translateAlternateColorCodes('&', Locale.infotitle));
-		for (String instructions : Locale.infoinstructions) {
-		    player.sendMessage(ChatColor.GREEN + ChatColor.translateAlternateColorCodes('&', instructions));
-		}
-		if (greenhouseInNow != null) {
-		    player.sendMessage(ChatColor.GOLD + Locale.infoinfo);
-		    // general.biome
-		    player.sendMessage(ChatColor.GREEN + Locale.generalbiome + ": " + Util.prettifyText(greenhouseInNow.getBiome().toString()));
-		    if (greenhouseInNow.getOwner() != null) {
-			Player owner = plugin.getServer().getPlayer(greenhouseInNow.getOwner());
-			if (owner != null) {
-			    player.sendMessage(ChatColor.YELLOW + Locale.generalowner + ": " + owner.getDisplayName() + " (" + owner.getName() + ")");
-			} else {
-			    player.sendMessage(ChatColor.YELLOW + Locale.generalowner + ": " + greenhouseInNow.getPlayerName());
-			}
-		    }
-		}
-		return true;
+        if (!(sender instanceof Player)) {
+            return false;
+        }
+        final Player player = (Player) sender;
+        // Check we are in the right world
+        if (!Settings.worldName.contains(player.getWorld().getName())) {
+            // notavailable
+            player.sendMessage(ChatColor.RED + Locale.generalnotavailable);
+            return true;
+        }
+        // Basic permissions check to even use /greenhouse
+        if (!VaultHelper.checkPerm(player, "greenhouses.player")) {
+            player.sendMessage(ChatColor.RED + Locale.errornoPermission);
+            return true;
+        }
+        /*
+         * Grab data for this player - may be null or empty
+         * playerUUID is the unique ID of the player who issued the command
+         */
+        final UUID playerUUID = player.getUniqueId();
+        final Greenhouse greenhouseInNow = players.getInGreenhouse(player);
+        switch (split.length) {
+        // /greenhouse command by itself
+        case 0:
+            if (greenhouseInNow==null || greenhouseInNow.getOwner().equals(playerUUID)) {
+                player.openInventory(plugin.getRecipeInv(player));
+                return true;
+            } else {
+                player.sendMessage(ChatColor.RED + Locale.errornotowner);
+                return true;
+            }
+        case 1:
+            // /greenhouse <command>
+            if (split[0].equalsIgnoreCase("help")) { 
+                player.sendMessage(ChatColor.GREEN + Locale.generalgreenhouses +" " + plugin.getDescription().getVersion() + " " + Locale.helphelp + ":");
+                player.sendMessage(ChatColor.YELLOW + "/" + label + " : " + ChatColor.WHITE + Locale.helpopengui);
+                player.sendMessage(ChatColor.YELLOW + "/" + label + " make: " + ChatColor.WHITE + Locale.helpmake);
+                player.sendMessage(ChatColor.YELLOW + "/" + label + " remove: " + ChatColor.WHITE + Locale.helpremove);
+                player.sendMessage(ChatColor.YELLOW + "/" + label + " info: " + ChatColor.WHITE + Locale.helpinfo);
+                player.sendMessage(ChatColor.YELLOW + "/" + label + " list: " + ChatColor.WHITE + Locale.helplist);
+                player.sendMessage(ChatColor.YELLOW + "/" + label + " recipe <number>: " + ChatColor.WHITE + Locale.helprecipe);
+                return true;
+            } else if (split[0].equalsIgnoreCase("list")) {
+                // List all the biomes that can be made
+                player.sendMessage(ChatColor.GREEN + Locale.listtitle);
+                player.sendMessage(Locale.listinfo);
+                int index = 1;
+                for (BiomeRecipe br : plugin.getBiomeRecipes()) {
+                    if (br.getFriendlyName().isEmpty()) {
+                        player.sendMessage(ChatColor.YELLOW + Integer.toString(index++) + ": " + Util.prettifyText(br.getBiome().toString()));
+                    } else {
+                        player.sendMessage(ChatColor.YELLOW + Integer.toString(index++) + ": " + br.getFriendlyName());
+                    }
+                }
+                return true;
+            } else if (split[0].equalsIgnoreCase("remove")) {
+                if (greenhouseInNow != null) {
+                    if (greenhouseInNow.getOwner().equals(playerUUID)) {
+                        player.sendMessage(ChatColor.RED + Locale.errorremoving);
+                        plugin.removeGreenhouse(greenhouseInNow);
+                        return true;
+                    }
+                    player.sendMessage(ChatColor.RED + Locale.errornotyours);
+                } else {
+                    player.sendMessage(ChatColor.RED + Locale.errornotinside); 
+                }
+                return true;
+            } else if (split[0].equalsIgnoreCase("make")) {
+                // Sets up a greenhouse
+                if (players.getInGreenhouse(player) != null) {
+                    // alreadyexists
+                    player.sendMessage(ChatColor.RED + Locale.erroralreadyexists);
+                    return true;
+                }
+                // Check if they are at their limit
+                if (plugin.players.isAtLimit(player)) {
+                    player.sendMessage(ChatColor.translateAlternateColorCodes('&', Locale.infonomore));
+                } else {
+                    // Try to make greenhouse
+                    Greenhouse g = plugin.tryToMakeGreenhouse(player);
+                    if (g == null) {
+                        // norecipe
+                        player.sendMessage(ChatColor.RED + Locale.errornorecipe);
+                        return true;
+                    }
+                    // Greenhouse is made
+                }
+                return true;
+            } else if (split[0].equalsIgnoreCase("info")) {
+                // Show some instructions on how to make greenhouses
+                player.sendMessage(ChatColor.GREEN + ChatColor.translateAlternateColorCodes('&', Locale.infotitle));
+                for (String instructions : Locale.infoinstructions) {
+                    player.sendMessage(ChatColor.GREEN + ChatColor.translateAlternateColorCodes('&', instructions));
+                }
+                if (greenhouseInNow != null) {
+                    player.sendMessage(ChatColor.GOLD + Locale.infoinfo);
+                    // general.biome
+                    player.sendMessage(ChatColor.GREEN + Locale.generalbiome + ": " + Util.prettifyText(greenhouseInNow.getBiome().toString()));
+                    if (greenhouseInNow.getOwner() != null) {
+                        Player owner = plugin.getServer().getPlayer(greenhouseInNow.getOwner());
+                        if (owner != null) {
+                            player.sendMessage(ChatColor.YELLOW + Locale.generalowner + ": " + owner.getDisplayName() + " (" + owner.getName() + ")");
+                        } else {
+                            player.sendMessage(ChatColor.YELLOW + Locale.generalowner + ": " + greenhouseInNow.getPlayerName());
+                        }
+                    }
+                }
+                return true;
 
-	    }
-	    break;
-	case 2:
-	    if (split[0].equalsIgnoreCase("make")) {
-		// Sets up a greenhouse for a specific biome
-		if (players.getInGreenhouse(player) != null) {
-		    // alreadyexists
-		    player.sendMessage(ChatColor.RED + Locale.erroralreadyexists);
-		    return true;
-		}
-		// Check if they are at their limit
-		if (plugin.players.isAtLimit(player)) {
-		    player.sendMessage(ChatColor.translateAlternateColorCodes('&', Locale.infonomore));
-		} else {
-		    // Check we are in a greenhouse
-		    Biome b = null;
-		    try {
-			b = Biome.valueOf(split[1].toUpperCase());
-		    } catch (Exception e) {
-			player.sendMessage(ChatColor.RED + Locale.errornorecipe);
-			return true;
-		    }
-		    if (b == null) {
-			player.sendMessage(ChatColor.RED + Locale.errornorecipe);
-			return true;
-		    }
-		    Greenhouse g = plugin.tryToMakeGreenhouse(player,b);
-		    if (g == null) {
-			// norecipe
-			player.sendMessage(ChatColor.RED + Locale.errornorecipe);
-			return true;
-		    }
-		    // Greenhouse is made
-		}
-		return true;
-	    } else if (split[0].equalsIgnoreCase("recipe")) {
-		int recipeNumber = 0;
-		try {
-		    recipeNumber = Integer.valueOf(split[1]);
-		} catch (Exception e) {
-		    player.sendMessage(ChatColor.RED + Locale.recipehint);
-		    return true;
-		}
-		List<BiomeRecipe> recipeList = plugin.getBiomeRecipes();
-		if (recipeNumber <1 || recipeNumber > recipeList.size()) {
-		    player.sendMessage(ChatColor.RED + Locale.recipewrongnumber.replace("[size]", String.valueOf(recipeList.size())));
-		    return true;
-		}
-		BiomeRecipe br = recipeList.get(recipeNumber-1);
-		player.sendMessage(ChatColor.GREEN + "[" + Util.prettifyText(br.getBiome().toString()) + " recipe]");
-		if (br.getWaterCoverage() == 0) {
-		    player.sendMessage(Locale.recipenowater);
-		} else if (br.getWaterCoverage() > 0) {
-		    player.sendMessage(Locale.recipewatermustbe.replace("[coverage]", String.valueOf(br.getWaterCoverage())));
-		}
-		if (br.getIceCoverage() == 0) {
-		    player.sendMessage(Locale.recipenoice);
-		} else if (br.getIceCoverage() > 0) {
-		    player.sendMessage(Locale.recipeicemustbe.replace("[coverage]", String.valueOf(br.getIceCoverage())));
-		}
-		if (br.getLavaCoverage() == 0) {
-		    player.sendMessage(Locale.recipenolava);
-		} else if (br.getLavaCoverage() > 0) {
-		    player.sendMessage(Locale.recipelavamustbe.replace("[coverage]", String.valueOf(br.getLavaCoverage())));
-		}
-		List<String> reqBlocks = br.getRecipeBlocks();
-		if (reqBlocks.size() > 0) {
-		    player.sendMessage(ChatColor.YELLOW + Locale.recipeminimumblockstitle);
-		    int index = 1;
-		    for (String list : reqBlocks) {
-			player.sendMessage((index++) + ": " + list);
-		    }
-		} else {
-		    player.sendMessage(ChatColor.YELLOW + Locale.recipenootherblocks);
-		}
-		return true;
-	    }
+            }
+            break;
+        case 2:
+            if (split[0].equalsIgnoreCase("make")) {
+                // Sets up a greenhouse for a specific biome
+                if (players.getInGreenhouse(player) != null) {
+                    // alreadyexists
+                    player.sendMessage(ChatColor.RED + Locale.erroralreadyexists);
+                    return true;
+                }
+                // Check if they are at their limit
+                if (plugin.players.isAtLimit(player)) {
+                    player.sendMessage(ChatColor.translateAlternateColorCodes('&', Locale.infonomore));
+                } else {
+                    // Check we are in a greenhouse
+                    Biome b = null;
+                    try {
+                        b = Biome.valueOf(split[1].toUpperCase());
+                    } catch (Exception e) {
+                        player.sendMessage(ChatColor.RED + Locale.errornorecipe);
+                        return true;
+                    }
+                    if (b == null) {
+                        player.sendMessage(ChatColor.RED + Locale.errornorecipe);
+                        return true;
+                    }
+                    Greenhouse g = plugin.tryToMakeGreenhouse(player,b);
+                    if (g == null) {
+                        // norecipe
+                        player.sendMessage(ChatColor.RED + Locale.errornorecipe);
+                        return true;
+                    }
+                    // Greenhouse is made
+                }
+                return true;
+            } else if (split[0].equalsIgnoreCase("recipe")) {
+                int recipeNumber = 0;
+                try {
+                    recipeNumber = Integer.valueOf(split[1]);
+                } catch (Exception e) {
+                    player.sendMessage(ChatColor.RED + Locale.recipehint);
+                    return true;
+                }
+                List<BiomeRecipe> recipeList = plugin.getBiomeRecipes();
+                if (recipeNumber <1 || recipeNumber > recipeList.size()) {
+                    player.sendMessage(ChatColor.RED + Locale.recipewrongnumber.replace("[size]", String.valueOf(recipeList.size())));
+                    return true;
+                }
+                BiomeRecipe br = recipeList.get(recipeNumber-1);
+                if (br.getFriendlyName().isEmpty()) {
+                player.sendMessage(ChatColor.GREEN + "[" + Util.prettifyText(br.getBiome().toString()) + " recipe]");
+                } else {
+                    player.sendMessage(ChatColor.GREEN + "[" + br.getFriendlyName() + " recipe]");
+                }
+                player.sendMessage(ChatColor.YELLOW + "Biome: " + Util.prettifyText(br.getBiome().toString()));        
+                if (br.getWaterCoverage() == 0) {
+                    player.sendMessage(Locale.recipenowater);
+                } else if (br.getWaterCoverage() > 0) {
+                    player.sendMessage(Locale.recipewatermustbe.replace("[coverage]", String.valueOf(br.getWaterCoverage())));
+                }
+                if (br.getIceCoverage() == 0) {
+                    player.sendMessage(Locale.recipenoice);
+                } else if (br.getIceCoverage() > 0) {
+                    player.sendMessage(Locale.recipeicemustbe.replace("[coverage]", String.valueOf(br.getIceCoverage())));
+                }
+                if (br.getLavaCoverage() == 0) {
+                    player.sendMessage(Locale.recipenolava);
+                } else if (br.getLavaCoverage() > 0) {
+                    player.sendMessage(Locale.recipelavamustbe.replace("[coverage]", String.valueOf(br.getLavaCoverage())));
+                }
+                List<String> reqBlocks = br.getRecipeBlocks();
+                if (reqBlocks.size() > 0) {
+                    player.sendMessage(ChatColor.YELLOW + Locale.recipeminimumblockstitle);
+                    int index = 1;
+                    for (String list : reqBlocks) {
+                        player.sendMessage((index++) + ": " + list);
+                    }
+                } else {
+                    player.sendMessage(ChatColor.YELLOW + Locale.recipenootherblocks);
+                }
+                return true;
+            }
 
-	}
-	return false;
+        }
+        return false;
     }
 
 
